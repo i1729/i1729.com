3e573fe71feed38c0b1caf0322fa752ee9032f13
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-30 22:25:44 -0600

    revert removing paths

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import '../assets/css/custom.css'
 
 import Home from './Home'
+/*
 import ProfileInfo from './Profile/Info'
 import Profile from './Profile'
 import Feed from './Feed'
@@ -12,12 +13,14 @@ import Folio from './Folio'
 import CredentialInfo from './Credential/Info'
 import Credential from './Credential'
 import Earn from './Earn'
+*/
 
 const history = createHistory()
 
 const App = () => 
   <Router history={history}>
     <Switch>
+{/*
       <Route path='/profile/:id' component={() => <ProfileInfo />} />
       <Route path='/profile' component={() => <Profile />} />
       <Route path='/feed' component={() => <Feed />} />
@@ -25,6 +28,7 @@ const App = () =>
       <Route path='/credential/:id' component={() => <CredentialInfo />} />
       <Route path='/credential' component={() => <Credential />} />
       <Route path='/earn' component={() => <Earn />} />
+*/}
       <Route path='/' component={() => <Home />} />
     </Switch>
   </Router>
