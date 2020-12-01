b14972afc73fe93f2d481b8a330bb5c556d4e86c
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-11-30 23:11:41 -0600

    completely start over for profile components

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,49 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Web3 from "web3";
-import { setupENS } from "@ensdomains/ui";
-import BounceLoader from "react-spinners/BounceLoader";
-import { Container, Row, Col } from 'react-grid-system';
+import React from 'react'
 
-export default () => {
-  const ENS_NAME = "1729" + ".eth";
-  const [loading, setLoading] = useState(true);
-  const [ensAddress, setEnsAddress] = useState(null);
-  const [ensUrl, setEnsUrl] = useState(null);
-
-  useEffect(async () => {
-    const { registrar, ens } = await setupENS()
-
-    const owner = await ens.getOwner(ENS_NAME)
-    setEnsAddress(owner);
-    const url = await ens.getText(ENS_NAME, 'url')
-    setEnsUrl(url);
-
-    setLoading(false);
-  }, []);
-
-  return (
-    <>
-      <h1>1729 Cryptoprofile</h1>
-      <p>This is the cryptoprofile for <a href="/">1729</a>. It resolves from <a href="https://app.ens.domains/">ENS</a>.</p>
-      {loading ?
-            <BounceLoader
-              css={"display: inline-block"}
-              size={50}
-              color={"#a00"}
-              loading={loading}
-            />
-      :
-        <Container>
-          <Row style={{ justifyContent: "center" }}>
-            <Col className="content" sm={12}>
-              <h3>{ENS_NAME}</h3>
-              { ensAddress ? <p>{ensAddress}</p> : null}
-              { ensUrl ? <p><a href={ensUrl}>{ensUrl}</a></p> : null }
-            </Col>
-          </Row>
-        </Container>
-      }
-    </>
-  )
-}
+export default () => 
+  <>
+    <h1>1729</h1>
+    <p>
+      This is <a href="/">1729</a>, you can goto <a href="/profile">cryptoprofile</a>, <a href="/feed">feed</a>, <a href="/folio">folio</a>, <a href="/credential">cryptocredential</a>, and <a href="/earn">earn</a>.
+    </p>
+  </>
--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,10 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import '../assets/css/custom.css'
 
-/*
-import ProfileInfo from './Profile/Info'
-import Profile from './Profile'
-*/
+const Loader = () =>  <BounceLoader css={"display: inline-block"} size={50} color={"#a00"} />
+const history = createHistory()
 
+const Profile = React.lazy(() => import('./Profile'));
 const CredentialInfo = React.lazy(() => import('./Credential/Info'));
 const Credential = React.lazy(() => import('./Credential'));
 const Feed = React.lazy(() => import('./Feed'));
@@ -16,16 +15,10 @@ const Folio = React.lazy(() => import('./Folio'));
 const Earn = React.lazy(() => import('./Earn'));
 const Home = React.lazy(() => import('./Home'));
 
-const history = createHistory()
-const Loader = () =>  <BounceLoader css={"display: inline-block"} size={50} color={"#a00"} />
-
 const App = () => 
   <Router history={history}>
     <Switch>
-{/*
-      <Route path='/profile/:id' component={() => <ProfileInfo />} />
-      <Route path='/profile' component={() => <Profile />} />
-*/}
+      <Route path='/profile' component={() => <Suspense fallback={Loader}><Profile /></Suspense>} />
       <Route path='/credential/:id' component={() => <Suspense fallback={Loader}><CredentialInfo /></Suspense>} />
       <Route path='/credential' component={() => <Suspense fallback={Loader}><Credential /></Suspense>} />
       <Route path='/feed' component={() => <Suspense fallback={Loader}><Feed /></Suspense>} />
